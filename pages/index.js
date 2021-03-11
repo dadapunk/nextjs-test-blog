928dbaae250586c63a4c5262054643e5b847adca
dadapunk <sebastian.v.a@riseup.net>
2021-03-11 19:43:45 -0300

    showing Json data into Cards

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,21 @@ export default function Home({blogs}) {
     <PageLayout>
     <AuthorIntro />
     <hr/>
-    {JSON.stringify(blogs)}
+    {/* {JSON.stringify(blogs)} */}
     <Row className="mb-5">
-      <Col md="10">
+      {/* <Col md="10">
         <CardListItem />
-      </Col>
-      <Col md="4">
-        <CardItem />
-      </Col>
+      </Col> */}
+      { blogs.map(blog =>
+        <Col key={blog.slug} md="4">
+          <CardItem 
+            title={blog.title}
+            subtitle={blog.subtitle}
+          />
+        </Col>
+        )
+      }
+
     </Row>
   </PageLayout>
     
@@ -31,6 +38,7 @@ export default function Home({blogs}) {
 // Provides props to you page
 // It will create static page
 export async function getStaticProps() {
+  console.log('Calling getStaticProps');
   const blogs = await getAllBlogs();
   return{
     props:{
