75e1c82e5e0c4de8f65b6c0a5593b854e3e8201e
dadapunk <sebastian.v.a@riseup.net>
2021-03-13 23:10:17 -0300

    authors

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+
 import { Row, Col } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
@@ -8,43 +9,37 @@ import { getAllBlogs } from 'lib/api';
 
 export default function Home({blogs}) {
   debugger
-  return(
+  return (
     <PageLayout>
-    <AuthorIntro />
-    <hr/>
-    {/* {JSON.stringify(blogs)} */}
-    <Row className="mb-5">
-      {/* <Col md="10">
-        <CardListItem />
-      </Col> */}
-      { blogs.map(blog =>
-        <Col key={blog.slug} md="4">
-          <CardItem 
-            title={blog.title}
-            subtitle={blog.subtitle}
-          />
-        </Col>
-        )
-      }
-
-    </Row>
-  </PageLayout>
-    
-
+      <AuthorIntro />
+      <hr/>
+      <Row className="mb-5">
+        {/* <Col md="10">
+          <CardListItem />
+        </Col> */}
+        { blogs.map(blog =>
+          <Col key={blog.slug} md="4">
+            <CardItem
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              image={blog.coverImage}
+            />
+          </Col>
+          )
+        }
+      </Row>
+    </PageLayout>
   )
 }
 
-// this function is calles during the build (build time)
-// Provides props to you page
-// It will create static page
 export async function getStaticProps() {
-  console.log('Calling getStaticProps');
+  const randomNumber = Math.random();
   const blogs = await getAllBlogs();
-  return{
-    props:{
-      blogs
+  return {
+    props: {
+      blogs,
+      randomNumber
     }
   }
- 
-
 }
